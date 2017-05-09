fafffc5a398c2ee34c12e2161ab4e7885a7c4a56
Jannis Hoppe <Jannis Hoppe>
2017-05-09 16:49:04 +0200

    added comments and fixed a bug

--- detector.py
+++ detector.py
@@ -9,10 +9,36 @@ import numpy as np
 class Detector(object):
 
     def __init__(self, img_shape, n_dense):
+        '''Initializes the detector network:
+        -----------
+
+        img_shape: tuple
+        A tuple denoting the image shape that the network is trained on. Should be of the form (height,width,channels)
+
+        n_dense: int
+        Number of units in the dense layers that are put on top of the pre trained ResNet.
+
+        Returns:
+        --------
+        Initialized detector network.
+        '''
         self.model = self.get_pre_resnet(img_shape, n_dense)
 
     def get_pre_resnet(self, input_shape, n_dense):
-        base_model = ResNet50(include_top=False, weights='imagenet', input_tensor=None, input_shape=(224,224,3))
+        '''Loads the pretrained Keras ResNet, adds 2 trainable dense layers and returns the compiled graph:
+        -----------
+
+        input_shape: tuple
+        A tuple denoting the image shape that the network is trained on. Should be of the form (height,width,channels)
+
+        n_dense: int
+        Number of units in the dense layers that are put on top of the pre trained ResNet.
+
+        Returns:
+        --------
+        Compiled detector network.
+        '''
+        base_model = ResNet50(include_top=False, weights='imagenet', input_tensor=None, input_shape=input_shape)
         x = base_model.output
         f_1 = Flatten()(x)
         n1 = BatchNormalization()(f_1)
@@ -34,10 +60,56 @@ class Detector(object):
         return res_net
 
     def train_on_batch(self, X,Y):
+        '''Same method as in the Keras API:
+        -----------
+
+        X: np.array
+        Numpy array containing the training data of shape (n_samples, height, width, channels)
+
+        Y: np.array
+        Corresponding labels for the training data.
+
+        Returns:
+        --------
+        None
+        '''
         self.model.train_on_batch(X.astype(np.float32), Y)
 
     def predict(self, X):
+        '''Returns rounded predictions on the given data:
+        -----------
+
+        X: np.array
+        Numpy array containing the data of shape (n_samples, height, width, channels)
+
+
+        Returns:
+        --------
+        Prediction: np.array
+        Numpy array containing the predictions with either 1 or 0
+        '''
         return np.round(self.model.predict(X)[:,1])
 
     def save_weights(self, path):
+        '''Same method as in the Keras API:
+        -----------
+
+        path: str
+        Filename of the model to be saved
+        Returns:
+        --------
+        None
+        '''
         self.model.save_weights(path)
+
+    def load_weights(self, path):
+        '''Same method as in the Keras API:
+        -----------
+
+        path: str
+        Filename of the model to be loaded
+        Returns:
+        --------
+        None
+        '''
+        self.model.load_weights(path)
